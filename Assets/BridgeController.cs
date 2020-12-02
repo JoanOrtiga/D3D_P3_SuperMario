d30bcd969be9bdca4f663ed418f94cb708766ab6
Marc <marcgaldo.97@gmail.com>
2020-12-02 17:24:08 +0100

    .

---
+++ Assets/BridgeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnCollisionEnter(Collision collision)
+    {
+        print(collision.gameObject.name);
+    }
+    public void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+
+        //print(hit.gameObject.name);
+        this.GetComponent<Rigidbody>().AddForceAtPosition(-hit.normal, hit.point);
+
+    }
+
+}
